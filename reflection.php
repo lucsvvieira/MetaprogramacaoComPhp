68067b4b68bb32473606933d392cbe279e473350
Lucas Vieira <vieira07lucas@gmail.com>
2022-12-20 14:28:01 -0300

    Obtendo informações da classe

--- reflection.php
+++ reflection.php
@@ -6,4 +6,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $reflectionClass = new ReflectionClass(ClasseExemplo::class);
 
-$objetoClasseExemplo = $reflectionClass->newInstanceArgs([1, 2]);
+$modifiers = $reflectionClass->getModifiers();
+$modifierNames = Reflection::getModifierNames($modifiers);
+
+$objetoClasseExemplo = $reflectionClass->newInstance();
+
+echo json_encode($objetoClasseExemplo);
